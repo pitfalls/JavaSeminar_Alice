08b5f6bf0b04821f7bb97d040b4d51256bd3fe3d
Alexander Frewein <alexander.frewein@student.tugraz.at>
2020-12-30 17:55:06 +0100

    erster commit

---
+++ Java_Grundlagen_Projekt/Java_Grundlagen/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        int a = 10;
+        int b = 10;
+        int result = a + b;
+        System.out.println("Hello, World " + result + "!");
+
+
+        
+        
+        int uno = -900;
+        int due = 1400;
+        int gogolori = uno + due;
+        System.out.println(gogolori);
+
+        float decimal_01 = 400.28f;
+        float decimal_22 = 98.2f; 
+        float decimal_3  = 88.348f;
+        float decimal_result = decimal_01 + decimal_3 - decimal_22;
+        System.out.println(decimal_result);
+
+        // Q: Why do I have to put a "f" after the decimal number at the end?
+
+        int current_year = 2020;
+        int birthyear = 1993;
+        int age = current_year - birthyear;
+        System.out.println(age);
+
+
+        
+    }
+}
